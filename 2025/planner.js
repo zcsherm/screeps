433b26327576a05041d03db4cb189b8eca59c4b3
zcsherm <165740629+zcsherm@users.noreply.github.com>
2025-06-04 16:25:16 -0700

    Create planner.js

---
+++ 2025/planner.js
@@ -0,0 +1,23 @@
+class Planner{
+    
+    constructor(room, room_level=0){
+        this.goals = [];
+        this.home_room = room;
+        this.level = level;
+        this.energy_max = 0;
+        this.current_energy = 0;
+    };
+
+    initialize{
+        /*
+        On initializing, we should find our spawn, find the sources, find the controller
+        */
+    };
+    first_level_planner(){
+        /*
+        *Goals at RCL one include:
+        * Upgrade to RCL 2
+        * Plan where to start building Extensions and roads
+        */
+    };
+};
